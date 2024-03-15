511deca204ab9a5a68e062517046762458226181
Furkan Güler <furkang2k20@outlook.com>
2024-03-15 14:55:02 +0300

    user cart implementation

---
+++ store.server/Controllers/UserController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using store.server.Infrastructure.Helpers;
+using store.server.Infrastructure.Models.Users;
+using store.server.Infrastructure.Data.Managers.Main;
+
+namespace store.server.Controllers
+{
+    [ApiController]
+    [Route("user")]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [Route("manage/cart")]
+        public async Task<IActionResult> ManageCart([FromBody] UserCart entity)
+        {
+            try
+            {
+                var UserID = AuthHelpers.CurrentUserID(HttpContext);
+                if (UserID > 0)
+                {
+                    entity.UserID = UserID;
+                    var result = await new UserManager().ManageCart(entity);
+                    return Ok(result);
+                }
+                return StatusCode(401, "Access denied");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(401, ex.Message);
+            }
+        }
+    }
+}
---
+++ store.server/Infrastructure/Models/Users/UserCart.cs
@@ -0,0 +1,11 @@
+﻿namespace store.server.Infrastructure.Models.Users
+{
+    public class UserCart
+    {
+        public int? ID { get; set; }
+        public int? UserID { get; set; }
+        public int? ProductID { get; set; }
+        public int? ColorID { get; set; }
+        public int? Amount { get; set; }
+    }
+}
